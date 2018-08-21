582236672add7f5db790d0d0d205d67afebf3358
sciepsilon <kalinda.pride@gmail.com>
2018-08-20 22:21:00 -0400

    defend against undefined/missing values

--- preprocessing/pfsx_utils.js
+++ preprocessing/pfsx_utils.js
@@ -4,12 +4,19 @@ to allow for use even if the pfsx file is missing from data/elan_files
 or even if the pfsx file failed to parse. */
 
 function getPrefList(listName, jsonData) {
-  if (jsonData == null) {
+  if (jsonData == null || jsonData['preferences'] == null || jsonData['preferences']['prefList'] == null) {
+    // jsonData==null or jsonData['preferences']==null means there wasn't a valid pfsx file.
+    // jsonData['preferences']['prefList']==null means there are e.g. tier color preferences, 
+    //     but all the list-type preferences are still set to the defaults.
     
     return [];
   }
   let prefLists = jsonData['preferences']['prefList'];
   for (let prefList of prefLists) {
+    if (prefList['$'] == null) {
+      // this shouldn't happen, but just skip this prefList and move to the next one 
+      continue;
+    }
     if (prefList['$']['key'] == listName) {
       
       return prefList['String'] || []; // assume all prefLists are lists of strings
@@ -24,7 +31,7 @@ function getTierOrder(jsonData) {
 }
 
 function getHiddenTiers(jsonData) {
-  return getPrefList('MultiTierViewer.HiddenTiers', jsonData);
+  return getPrefList('MultiTierViewer.HiddenTiers', jsonData) || [];
 }
 
 module.exports = {
