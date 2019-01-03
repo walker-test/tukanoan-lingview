ade89f141272ed6067405b71326ae2fd646c6403
Thomas Ottaway <tottaway123@gmail.com>
2019-01-03 16:37:46 -0500

    Old version of search w/o tiers

--- jsx/App/Search.jsx
+++ jsx/App/Search.jsx
@@ -1,39 +1,28 @@
 import React from 'react';
 import Fuse from 'fuse.js';
 import { Sentence } from './Stories/Story/Display/Sentence.jsx';
-import { notDeepEqual } from 'assert';
 
 
 export class Search extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {searchResults: [],
-                      showFields: []};
+        this.state = {searchResults: []};
         this.list = this.props.search_index;
-        this.handleSearchInput = this.handleSearchInput.bind(this);
-    }
-    getFields() {
-        let fields = []
-        document.getElementsByName("fields").forEach(function (e) {
-            if (e.checked) fields.push(`dependents.${e.id}.value`);
-        });
-        return fields
+        this.handleInput = this.handleInput.bind(this);
     }
 
     runSearch (query) {
-        const fields = this.getFields();
-        console.log(fields);
         var options = {
             shouldSort: true,
             findAllMatches: true,
             tokenize: true,
-            matchAllTokens: true,
+            matchAllTokens: true,  
             threshold: 0,
             location: 0,
             distance: 100,
             maxPatternLength: 32,
             minMatchCharLength: 1,
-            keys: fields
+            keys: ["dependents.values.value"]
         };
         console.log("running search");
         var fuse = new Fuse(this.list, options); // "list" is the item array
@@ -41,48 +30,36 @@ export class Search extends React.Component {
         return fuse.search(query);
     }
 
-    handleSearchInput(event) {
-        // console.log("This?");
-        // console.log(this);
-        // console.log("Query:");
-        // console.log(event.target.value);
+    handleInput(event) {
+        console.log("This?");
+        console.log(this);
+        console.log("Query:");
+        console.log(event.target.value);
         const searchResult = this.runSearch(event.target.value);
-        // console.log("Search result:");
-        // console.log(searchResult);
+        console.log("Search result:");
+        console.log(searchResult);
         const displayTable = document.getElementById("searchResults");
-        displayTable.innerHTML = ""; //(DON'T DO THIS)
-        // searchResults = [];
+        // displayTable.innerHTML = ""; (DON'T DO THIS)
+        searchResults = [];
         for (var i = 0, j = searchResult.length; i < j; i++) {
-                // const speaker = 'speaker' in searchResult[i]
-                displayTable.innerHTML += `<p>${JSON.stringify(searchResult[i].text)}</p>`;
+            if ('speaker' in searchResult[i]) {
+                let component = (<Sentence sentence={searchResult[i]} true />);
+                searchResults.push(component);
+            } else {
+                let component = (<Sentence sentence={searchResult[i]} false />);
+                searchResults.push(component);
+            }
         }
-        // this.setState({searchResults: searchResults});
+        this.setState({searchResults: searchResults});
     }
-
     render() {
         console.log("rendering...");
         let results = this.state.searchResults;
         return (
           <div>
-            <label for="searchInput">Search database:</label>
-                <input id="searchInput" onInput={this.handleSearchInput} type="text"/>
-            <br />
-            <label>Select Fields   </label>
-            <label>Tier 1</label>
-                <input id="T1" onChange={this.handleSearchInput} name="fields" type="checkbox" />
-            <label>Tier 2</label>
-                <input id="T2" onChange={this.handleSearchInput} name="fields" type="checkbox" />
-            <label>Tier 3</label>
-                <input id="T3" onChange={this.handleSearchInput} name="fields" type="checkbox" />
-            <label>Tier 4</label>
-                <input id="T4" onChange={this.handleSearchInput} name="fields" type="checkbox" />
-            <label>Tier 5</label>
-                <input id="T5" onChange={this.handleSearchInput} name="fields" type="checkbox" />
-            <label>Tier 6</label>
-                <input id="T6" onChange={this.handleSearchInput} name="fields" type="checkbox" />
+            <label for="searchInput">Search database:</label> <input id="searchInput" onInput={this.handleInput} type="text"/>
             <div id="searchResults">{results}</div>
           </div>
       )
     };
 }
-
