72375b2a1514338752021c01f769cf2dce7f8023
tottaway <tottaway123@gmail.com>
2019-04-11 12:37:02 -0400

    only rebuild fuse when neccessary

--- jsx/App/Search.jsx
+++ jsx/App/Search.jsx
@@ -8,7 +8,7 @@ export class Search extends React.Component {
         super(props);
         this.state = { "searchResults": [] };
         this.list = this.props.search_index;
-        this.runSearch = this.throttle(this.search, 500, this);
+        this.runSearch = this.throttle(this.search, 300, this);
     }
 
     throttle(fn, threshhold, scope) {
@@ -20,16 +20,17 @@ export class Search extends React.Component {
         var deferTimer;
         return function () {
             var context = scope || this;
+
+            var args = arguments;
             
             if (deferTimer) clearTimeout(deferTimer);
             deferTimer = setTimeout(function () {
-                return fn.apply(context);
+                return fn.apply(context, args);
             }, threshhold);
         };
     }
 
-    search() {
-        let query = document.getElementById("searchInput").value;
+    build_fuse() {
         let fields = [];
         document.getElementsByName("fields").forEach(function (e) {
             if (e.checked) fields.push(`dependents.${e.id}.value`);
@@ -50,9 +51,18 @@ export class Search extends React.Component {
         };
         
         console.log("running search over: " + fields);
-        let fuse = new Fuse(this.list, options); // "list" is the item array
-        console.log("Made a fuse...");
-        let searchResult = fuse.search(query).slice(0, 25);
+        return new Fuse(this.list, options) // "list" is the item array
+    }
+
+    search(rebuild=true) {
+        if (rebuild || !this.fuse) this.fuse = this.build_fuse();
+        let input = document.getElementById("searchInput");
+        if (input) {
+            var query = input.value;
+        } else {
+            return;
+        }
+        let searchResult = this.fuse.search(query).slice(0, 25);
 
         console.log("Search result:");
         console.log(searchResult);
@@ -71,25 +81,29 @@ export class Search extends React.Component {
         this.setState({ "searchResults": searchResults });
     }
 
+    handleInputChange() {
+        this.runSearch(false);
+    }
+
     render() {
         let results = this.state.searchResults;
         console.log("rendering...");
         return (
             <div id="searchForm">
-                <label for="searchInput">Search database:</label> <input id="searchInput" onInput={this.runSearch} type="text" />
+                <label for="searchInput">Search database:</label> <input id="searchInput" onChange={this.handleInputChange.bind(this)} type="text" />
                 <br />
                 <label>Select Fields:   </label>
-                <input id="H1_Transcripción" name="fields" type="checkbox" onInput={this.runSearch} defaultChecked />
+                <input id="H1_Transcripción" name="fields" type="checkbox" onChange={this.search.bind(this)} defaultChecked />
                 <label>H1_Transcripción</label>&nbsp;&nbsp;
-                <input id="H2_Transcripción" name="fields" type="checkbox" onInput={this.runSearch} defaultChecked />
+                <input id="H2_Trarscripción" name="fields" type="checkbox" onChange={this.search.bind(this)} defaultChecked />
                 <label>H2_Transcripción</label>&nbsp;&nbsp;
-                <input id="H1_Maya" name="fields" type="checkbox" onInput={this.runSearch} defaultChecked />
+                <input id="H1_Maya" name="fields" type="checkbox" onChange={this.search.bind(this)} defaultChecked />
                 <label>H1_Maya</label>&nbsp;&nbsp;
-                <input id="H2_Maya" name="fields" type="checkbox" onInput={this.runSearch} defaultChecked />
+                <input id="H2_Maya" name="fields" type="checkbox" onChange={this.search.bind(this)} defaultChecked />
                 <label>H2_Maya</label>&nbsp;&nbsp;
-                <input id="H1_Traducción" name="fields" type="checkbox" onInput={this.runSearch} defaultChecked />
+                <input id="H1_Traducción" name="fields" type="checkbox" onChange={this.search.bind(this)} defaultChecked />
                 <label>H1_Traducción</label>&nbsp;&nbsp;
-                <input id="H2_Traducción" name="fields" type="checkbox" onInput={this.runSearch} defaultChecked />
+                <input id="H2_Traducción" name="fields" type="checkbox" onChange={this.search.bind(this)} defaultChecked />
                 <label>H2_Traducción</label>
                 <br />
                 <div id="searchResults">{results}</div>
