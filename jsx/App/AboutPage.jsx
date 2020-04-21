d07c90d6958773fcddb9d7cce06cc7acd9f48f04
Jacob Zimmerman <jczimm@jczimm.com>
2020-04-21 00:35:31 -0400

    add About and Glossing Abbreviations pages + tabs

---
+++ jsx/App/AboutPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class AboutPage extends React.Component {
+    render() {
+        return (
+           <main className="content-wrapper">
+               <header>
+                   <h1>About</h1>
+                </header>
+               <article>
+                   <p>Lorem ipsum dolor sit amet</p>
+                </article>
+           </main>
+        );
+    }
+}
---
+++ jsx/App/GlossingAbbrevsRefPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class GlossingAbbrevsRefPage extends React.Component {
+    render() {
+        return (
+           <main className="content-wrapper">
+               <header>
+                   <h1>Glossing Abbreviations</h1>
+                </header>
+               <article>
+                   <p>Lorem ipsum dolor sit amet</p>
+                </article>
+           </main>
+        );
+    }
+}
