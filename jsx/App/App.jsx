7820d776740afa2fbd72304ee68d168832da7fc1
Jacob Zimmerman <jczimm@jczimm.com>
2020-04-23 21:39:41 -0400

    remove about page and abbrevs page stubs; not core

--- jsx/App/App.jsx
+++ jsx/App/App.jsx
@@ -4,8 +4,6 @@ import { Route } from 'react-router-dom';
 import { LandingPage } from './LandingPage.jsx';
 import { StoryIndex } from './StoryIndex.jsx';
 import { Stories } from './Stories/Stories.jsx';
-import { AboutPage } from './AboutPage.jsx';
-import { GlossingAbbrevsRefPage } from './GlossingAbbrevsRefPage.jsx';
 
 export function App({ data }) {
     return (
@@ -13,8 +11,6 @@ export function App({ data }) {
             <Route exact path="/" render={props => <LandingPage/>} />
             <Route exact path="/index" render={props => <StoryIndex index={data.index} />} />
             <Route path="/story" render={props => <Stories stories={data.stories} />} />
-            <Route path="/about" render={props => <AboutPage />} />
-            <Route path="/abbrevs" render={props => <GlossingAbbrevsRefPage />} />
         </div>
     );
 }
