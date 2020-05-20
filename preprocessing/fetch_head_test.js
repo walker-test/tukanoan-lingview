a16b864ccd16824831c14334a7d30e8c83a5a2ae
Jacob Zimmerman <jczimm@jczimm.com>
2020-05-20 18:43:19 -0400

    switch from gdrive remote media hosting to webserver remote media hosting

---
+++ preprocessing/fetch_head_test.js
@@ -0,0 +1,17 @@
+const fetch = require('fetch-retry')(require('isomorphic-fetch'));
+const { URL } = require('url');
+
+async function fetchHeadTest(url) {
+  const urlEncoded = new URL(url).href; // without encoding the url, the fetch fails if the url includes any letters with diacritics
+  const response = await fetch(urlEncoded, {
+    method: 'HEAD',
+    retries: 5,
+    retryDelay: 1000
+  });
+  if (!response.ok) {
+    console.warn('unexpected response', response.statusText);
+  }
+  return response.ok;
+}
+
+module.exports = () => fetchHeadTest;
