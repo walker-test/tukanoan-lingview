fa48448c19c571f92ac485cfaa6bd22a9f8765cc
Valerie Aguilar Dellisanti <valerieaguilardellisanti@valeries-air.devices.brown.edu>
2020-07-21 20:21:25 -0400

    Add About Page & Glossary

--- jsx/App/App.jsx
+++ jsx/App/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import id from 'shortid';
 import { Route } from 'react-router-dom';
 import { LandingPage } from './LandingPage.jsx';
+import { AboutPage } from './AboutPage.jsx';
+import { GlossaryPage } from './GlossaryPage.jsx';
 import { StoryIndex } from './StoryIndex.jsx';
 import { Stories } from './Stories/Stories.jsx';
 
@@ -9,6 +11,8 @@ export function App({ data }) {
     return (
         <div>
             <Route exact path="/" render={props => <LandingPage/>} />
+            <Route exact path="/about" render={props => <AboutPage/>} />
+            <Route exact path="/glossary" render={props => <GlossaryPage/>} />
             <Route exact path="/index" render={props => <StoryIndex index={data.index} />} />
             <Route path="/story" render={props => <Stories stories={data.stories} />} />
         </div>
---
+++ jsx/App/Glossary.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function GlossaryPage() {
+  return (
+      <div>
+          <p>Input glossary here :) </p>
+          <p>To customize this welcome text, edit the jsx/App/GlossaryPage.jsx file, then run webpack for your changes to take effect. </p>
+      </div>
+  );
+}
