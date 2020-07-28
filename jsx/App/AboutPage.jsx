58fa88d9a8a5446b7842a60f42d05f5b8a70d261
maksymilian-dabkowski <maksymilian.michal.dabkowski@gmail.com>
2020-07-28 08:44:58 -0400

    margin

--- jsx/App/AboutPage.jsx
+++ jsx/App/AboutPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function AboutPage() {
   return (
-      <div>
+      <div style={{margin: "0.25in"}}>
           <p>Input content here :) </p>
           <p>To customize this welcome text, edit the jsx/App/AboutPage.jsx file, then run webpack for your changes to take effect. </p>
       </div>
