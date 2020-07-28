15cb781bb9c3c212ed9142c477fc2baf3f30d1aa
maksymilian-dabkowski <maksymilian.michal.dabkowski@gmail.com>
2020-07-28 08:45:42 -0400

    margin

--- jsx/App/GlossaryPage.jsx
+++ jsx/App/GlossaryPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function GlossaryPage() {
   return (
-      <div>
+      <div style={{margin: "0.25in"}}>
           <p>Input glossary here :) </p>
           <p>To customize this welcome text, edit the jsx/App/GlossaryPage.jsx file, then run webpack for your changes to take effect. </p>
       </div>
