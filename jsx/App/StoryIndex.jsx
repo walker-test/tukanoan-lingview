e122bbda4cb23fd246953dff4b8b1020e2d4d8ae
hollyyuqizheng <holly_zyq@126.com>
2020-08-20 11:54:07 -0400

    Delete commented out code

--- jsx/App/StoryIndex.jsx
+++ jsx/App/StoryIndex.jsx
@@ -57,17 +57,6 @@ export class StoryIndex extends React.Component {
     render() {
         return (
             <div id="index">
-
-                {/* <div class="dataTables_length" id="indexTable_length">
-                    <label>Show Entries</label>
-                    <select name="indexTable_length" aria-controls="indexTable" class>
-                        <option value="10">10</option>
-                        <option value="25">25</option>
-                        <option value="50">50</option>
-                        <option value="1" selected>1</option>
-                    </select>
-                </div> */}
-
                 <table id="indexTable"></table>
             </div>
         );
