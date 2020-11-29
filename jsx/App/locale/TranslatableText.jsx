bc56ac7d0cd428e95c7f1105e28ff30dbf54ff63
elisharf <elibsharf@gmail.com>
2020-11-29 13:31:04 -0800

    Add flexibility to TranslatableText component (#71)
    * Add ability to return whatever language is in the dictionary for TranslateableText component.
    
    Co-authored-by: Eli Sharf <esharf@google.com>
    Co-authored-by: mashton <miriamashton3@gmail.com>

--- jsx/App/locale/TranslatableText.jsx
+++ jsx/App/locale/TranslatableText.jsx
@@ -9,6 +9,11 @@ export const TranslatableText = ({ dictionary }) => {
   const { locale } = useContext(LocaleContext);
   if (dictionary[locale] === undefined) {
     console.error(`[TranslatableText] No translation for ${locale} in provided dictionary`);
+    // Try to return default language, if that is undefined, then just return
+    // whatever the first element of the dictionary is.
+    if (dictionary[DEFAULT_LOCALE] === undefined) {
+      return dictionary[Object.keys(dictionary)[0]];
+    }
     return dictionary[DEFAULT_LOCALE];
   }
   return dictionary[locale];
