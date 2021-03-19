42fe84681aec738ecc3e04441f4b74cbbe37d310
hollyyuqizheng <holly_zyq@126.com>
2021-03-18 20:16:04 -0400

    Complete basic formatting with gb4e-modified package

--- jsx/App/Stories/Story/Display/TextFormatButton.jsx
+++ jsx/App/Stories/Story/Display/TextFormatButton.jsx
@@ -7,7 +7,7 @@ export function TextFormatButton({ sentences, metadata }) {
     */
     function processSentences() {
         const dependents = sentences[0]["dependents"];
-        //console.log(dependents);
+
         const aingaeWordList = dependents[0]["values"];
         const morphemeList = dependents[1]["values"];
         const glossList = dependents[3]["values"];
@@ -15,8 +15,6 @@ export function TextFormatButton({ sentences, metadata }) {
         const morphAndGloss = organizeWords(aingaeWordList, morphemeList, glossList);
         const morphemeMap = morphAndGloss["morphemes"];
         const glossMap = morphAndGloss["gloss"];
-        //console.log(morphemeMap);
-        //console.log(glossMap);
 
         const sentenceTranslation = getSentenceTranslation();     
         const title = getTitle();
@@ -83,16 +81,18 @@ export function TextFormatButton({ sentences, metadata }) {
         return title; 
     }
 
-    /* Convert a sentence into LaTeX format with gb4e package style. */
+    /* Convert a sentence into LaTeX format with gb4e-modified package style. */
     function convertToLatex(material) {
         // Some literal symbols used as latex markups.
-        const begin = "\\begin{exe} \n  \\ex ";
+        const begin = "\\begin{exe} \n  \\ex \\label{example} \n  ";
         const end = "\\end{exe}";
         
         const morphLines = getMorphemeLines(material["morphemes"])
-        const glossLines = getMorphologicalAnalysisLine(material["gloss"]);
+        const glossLine = getMorphologicalAnalysisLine(material["gloss"]);
         const translationLine = getTranslationLatexLine(material["sentenceTranslation"]);
-        const toDisplay = begin + morphLines + glossLines + translationLine + end;
+        // Replace _ with \_ so that it is recognized as underscore in LaTeX
+        const storyTitle = material["title"].replace(/_/g, "\\_") + "\n"; 
+        const toDisplay = begin + morphLines + glossLine + translationLine + storyTitle + end;
         console.log(toDisplay);
         return toDisplay; 
     }
@@ -100,7 +100,7 @@ export function TextFormatButton({ sentences, metadata }) {
     /* Combines the glossing and morphological analysis into their corresponding lines. */
     function getMorphemeLines(morphemes) {
         const morphemeStart = "\\gll";
-        const morphemeEnd = ".\\\\ \n";
+        const morphemeEnd = "\\\\ \n  ";
         
         let wordList = []; // This will contain the complete sentence without - or == 
         let morphemeList = [morphemeStart]; // This has each word decomposed into suffices and clitics.
@@ -112,7 +112,7 @@ export function TextFormatButton({ sentences, metadata }) {
         }
         morphemeList.push(morphemeEnd);
         
-        return wordList.join(" ") + " \\\\\n" + morphemeList.join(" ");
+        return wordList.join(" ") + " \\\\\n  " + morphemeList.join(" ");
     }
 
     function getMorphologicalAnalysisLine(gloss) {
@@ -126,16 +126,20 @@ export function TextFormatButton({ sentences, metadata }) {
                 for (const [id, glossItem] of Object.entries(glossItems)) {
                     // Only the suffices and clitics need \textsc
                     if (isSuffix(glossItem)) {
-                        // TODO: need to lowercase he clitics
-                        glossForThisWord.push(textscStart + glossItem + textscClose);
+                        glossForThisWord.push(textscStart + glossItem.toLowerCase() + textscClose);
                     } else {
                         glossForThisWord.push(glossItem); 
                     } 
                 }
-                glossList.push(glossForThisWord.join(""));
+                // Reason for using the replace with "_" is that some glossed word is two words in
+                // the translation, but two words with a space in between will be recognized as two
+                // separate glossed word by the LaTeX package, so adding the underscore makes sure 
+                // that a phrase made up with multiple words can still be grouped together after being rendered in LaTeX. 
+                glossList.push(glossForThisWord.join("").replace(" ", "\\_"));
+                console.log(glossList);
             }
         }
-        glossList.push("\\\\ \n");
+        glossList.push("\\\\ \n  ");
 
         return glossList.join(" ");
     }
@@ -143,14 +147,14 @@ export function TextFormatButton({ sentences, metadata }) {
     /* Puts the sentence translation into LaTeX format. */
     function getTranslationLatexLine(sentence) {
         const translationStart = "\\glt `";
-        const translationEnd = "'\n";
+        const translationEnd = "' \\\\ \n  ";
         return translationStart + sentence + translationEnd;
     }
 
     /* Checks if an item is a suffix or clitic. */
     function isSuffix(item) {
-        return item.startsWith("=") || item.startsWith("-");
-        // TODO: also need to check if the item is complete capitalized
+        // Suffix or clitic starts with = or -, or the entire word is capitalized.
+        return item.startsWith("=") || item.startsWith("-") || item === item.toUpperCase();
     }
 
     /* Displays the created material in a popup window. */
@@ -163,7 +167,7 @@ export function TextFormatButton({ sentences, metadata }) {
         e.preventDefault();
         const processedMaterial = processSentences();
         const latexLines = convertToLatex(processedMaterial);
-        //displayInPopup(latexLines);
+        displayInPopup(latexLines);
     }
 
     return (
