5bee6a6d9af7c40a5c8a7cb8b973146b1f893e84
Walker White <wwhite@walkercwhite.com>
2024-02-16 00:58:09 -0700

    Fixed a bug in displaying search

--- jsx/App/Header.jsx
+++ jsx/App/Header.jsx
@@ -65,7 +65,7 @@ export function Header() {
           <Submissions />
         </Route>
         <Route exact path="/search">
-          <Submissions />
+          <Search />
         </Route>
 
       </div>
